d308354c6c6e4df5961775e9c050c4dfc34b325e
pjmlp <pjmlp@progtools.org>
2014-02-09 20:24:39 +0100

    Fixed Android macro. Added support for make_unique() with gcc.

--- jni/src/glos.h
+++ jni/src/glos.h
@@ -19,13 +19,23 @@
 #ifndef GLOS_H_
 #define GLOS_H_
 
-#ifdef ANDROID 
+#ifdef __ANDROID__
     #include <EGL/egl.h>
     #include <GLES/gl.h>
+
+	#include <memory>
 
     // No Glew on Android, always return true
     #define InitializeGlew() (true)
 
+   // GCC still only has C++11 support, need to add make_unique();
+    namespace std {
+	template<typename T, typename ...Args>
+	std::unique_ptr<T> make_unique( Args&& ...args )
+	{
+		return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
+	}
+    }
 #else
     #include <GL/glew.h>
 
