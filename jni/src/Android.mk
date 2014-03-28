f21d66a8755363cffaca3eae899347f7432a3e90
pjmlp <pjmlp@progtools.org>
2014-03-28 21:12:02 +0100

    Updated linked libraries and list of files to compile.

--- jni/src/Android.mk
+++ jni/src/Android.mk
@@ -13,11 +13,11 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/$(GLM_PATH)
 # Application files
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
 	townsdl.cpp app.cpp world.cpp mesh.cpp diskprimitive.cpp boxprimitive.cpp \
-	arcprimitive.cpp roofprimitive.cpp arctunnelprimitive.cpp primitive.cpp \
+	arcprimitive.cpp roofprimitive.cpp arctunnelprimitive.cpp primitive.cpp effect.cpp \
 	tunnelprimitive.cpp windmillprimitive.cpp cylinderprimitive.cpp sailprimitive.cpp
 
 LOCAL_SHARED_LIBRARIES := SDL2
 
-LOCAL_LDLIBS := -lGLESv1_CM -llog
+LOCAL_LDLIBS := -lGLESv2 -llog
 
 include $(BUILD_SHARED_LIBRARY)
