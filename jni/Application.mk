6c0011dcb3baad68bb636881f8bd6563af9a0604
pjmlp <pjmlp@progtools.org>
2014-12-13 00:03:27 +0100

    Updated the compiler to gcc 4.9 and C++14 mode.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,6 +1,6 @@
 # Application build flags
-APP_CPPFLAGS += -std=c++11 -DGLM_FORCE_RADIANS -Werror -Wall  -isystem jni/glm
+APP_CPPFLAGS += -std=c++14 -DGLM_FORCE_RADIANS -Werror -Wall  -isystem jni/glm
 APP_STL := gnustl_static
 APP_ABI := armeabi-v7a x86
 
-NDK_TOOLCHAIN_VERSION := 4.8
+NDK_TOOLCHAIN_VERSION := 4.9
